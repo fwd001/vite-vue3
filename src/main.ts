59ff1cb58458dea6c74cf47f026f5d3795a02d0a
fuwedong <fuwendong5@outlook.com>
2022-07-29 12:32:36 +0800

    feat: 改造main 引用顺序

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
 import router from "@/router/index";
 import antd from "@/common/ant-design";
 import mitt from "@/common/event-bus";
 import { createPinia } from "pinia";
+import "./style.css";
 import "@/assets/js/polyfill";
 import "@/assets/css/public.less";
 import "@/assets/css/common.less";
-
 import Editor from "@tinymce/tinymce-vue";
 
 const app = createApp(App);
