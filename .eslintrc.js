778892645f68329b8ed130f716d2063ef61ec4f7
fuwedong <fuwendong5@outlook.com>
2022-07-29 12:54:31 +0800

    feat: 添加eslint 添加格式化工具

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {}, // Allows for the parsing of JSX
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {},
+};
---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+// 具体配置可以参考 https://prettier.io/docs/en/options.html
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true, // 未尾逗号
+  vueIndentScriptAndStyle: true,
+  singleQuote: true, // 单引号
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  trailingComma: 'es5', // 未尾分号
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  arrowParens: 'always',
+  insertPragma: false,
+  requirePragma: false,
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf',
+};
