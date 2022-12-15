e09aeafde7900737d7f21b639f57e0bf66f1a4e0
fuwedong <fuwendong5@outlook.com>
2022-12-15 12:29:20 +0800

    feat: 升级包｜添加基础配置｜修复打包问题

--- src/plugins/tinymce.ts
+++ src/plugins/tinymce.ts
@@ -1,9 +1,6 @@
 import type { App } from 'vue'
 import Editor from '@tinymce/tinymce-vue'
-import '' // 插入上传图片插件
 
 export function setupTinymce(app: App<Element>) {
   app.component('TinymceEditor', Editor)
-  // 添加到全局
-  // app.config.globalProperties.$antdIcons = antdIcons
 }
