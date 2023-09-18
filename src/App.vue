65b08d290125bb482841eb176711c836332e3851
fuwedong <fuwendong5@outlook.com>
2023-09-18 12:13:00 +0800

    feat: 添加配置

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <script lang="ts" setup>
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
 import { theme } from 'ant-design-vue'
+import { useConfigStore } from '@/store'
+
+import axios from 'axios'
+import { useRequest } from 'vue-request'
 
 defineOptions({
   name: 'App',
 })
 
 const locale = zhCN
+
+const configStore = useConfigStore()
+// 获取config.json
+const { loading: configLoading } = useRequest(
+  async () => {
+    const res = await axios.get(`${import.meta.env.VITE_BASE}config.json`)
+    return res.data
+  },
+  {
+    onSuccess(res) {
+      configStore.setConfig(res)
+    },
+  },
+)
 </script>
 
 <template>
@@ -23,7 +41,7 @@ const locale = zhCN
       },
     }"
     :locale="locale">
-    <router-view></router-view>
+    <router-view v-if="!configLoading"></router-view>
   </a-config-provider>
 </template>
 
