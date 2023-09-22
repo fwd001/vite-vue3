f138227d24fef5a66be897ab3b578f394f4972da
fuwedong <fuwendong5@outlook.com>
2023-09-22 16:20:09 +0800

    feat: 添加antd样式兼容性问题

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { legacyLogicalPropertiesTransformer } from 'ant-design-vue'
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
 import { theme } from 'ant-design-vue'
 import { useConfigStore } from '@/store'
@@ -27,21 +28,23 @@ const { loading: configLoading } = useRequest(
 </script>
 
 <template>
-  <a-config-provider
-    :theme="{
-      algorithm: theme.defaultAlgorithm,
-      token: {
-        // colorPrimary: '#00b96b',
-      },
-      components: {
-        Button: {
-          colorPrimary: '#00b96b',
+  <a-style-provider hash-priority="high" :transformers="[legacyLogicalPropertiesTransformer]">
+    <a-config-provider
+      :theme="{
+        algorithm: theme.defaultAlgorithm,
+        token: {
+          // colorPrimary: '#00b96b',
         },
-      },
-    }"
-    :locale="locale">
-    <router-view v-if="!configLoading"></router-view>
-  </a-config-provider>
+        components: {
+          Button: {
+            colorPrimary: '#00b96b',
+          },
+        },
+      }"
+      :locale="locale">
+      <router-view v-if="!configLoading"></router-view>
+    </a-config-provider>
+  </a-style-provider>
 </template>
 
 <style scoped></style>
