96943032c5eac5cfa655f85b796e8c0fcc23919c
fuwedong <fuwendong5@outlook.com>
2024-01-24 11:23:00 +0800

    feat: 新版初始化

---
+++ src/layouts/default/sider/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <MixSider v-if="getIsMixSidebar" />
+  <Sider v-else />
+</template>
+
+<script lang="ts" setup>
+  import Sider from './LayoutSider.vue';
+  import MixSider from './MixSider.vue';
+
+  import { useMenuSetting } from '@/hooks/setting/useMenuSetting';
+
+  defineOptions({ name: 'SiderWrapper' });
+
+  const { getIsMixSidebar } = useMenuSetting();
+</script>
+<style lang="less">
+  @prefix-cls: ~'@{namespace}-layout-sider-wrapper';
+
+  .@{prefix-cls} {
+    .ant-drawer-body {
+      height: 100vh;
+      padding: 0;
+    }
+
+    .ant-drawer-header-no-title {
+      display: none;
+    }
+  }
+</style>
