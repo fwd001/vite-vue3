8ef17c92d1776797361f0ee9db632c6260a89153
fuwedong <fuwendong5@outlook.com>
2024-02-01 14:57:21 +0800

    feat: 解决项目兼容性问题

--- internal/vite-config/src/plugins/index.ts
+++ internal/vite-config/src/plugins/index.ts
@@ -1,3 +1,4 @@
+import legacy from '@vitejs/plugin-legacy';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { type PluginOption } from 'vite';
@@ -32,6 +33,35 @@ async function createPlugins({ isBuild, root, enableMock, compress, enableAnalyz
 
   // vite-plugin-purge-icons
   vitePlugins.push(purgeIcons());
+  vitePlugins.push(
+    // 兼容性配置
+    legacy({
+      targets: ['defaults', 'chrome 65', 'not dead'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      renderLegacyChunks: true,
+      modernPolyfills: [
+        'es.symbol',
+        'es.array.filter',
+        'es.promise',
+        'es.promise.finally',
+        'es.promise.all-settled',
+        'es/map',
+        'es/set',
+        'es.array.flat-map',
+        'es.array.for-each',
+        'es.object.define-properties',
+        'es.object.define-property',
+        'es.object.get-own-property-descriptor',
+        'es.object.get-own-property-descriptors',
+        'es.object.keys',
+        'es.object.to-string',
+        'web.dom-collections.for-each',
+        'es.global-this',
+        'esnext.global-this',
+        'esnext.string.match-all',
+      ],
+    }),
+  );
 
   // The following plugins only work in the production environment
   if (isBuild) {
