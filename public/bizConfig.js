8c69b054ec00afc1f15401fbbd9e7bf89518db99
dev-172.16.10.202 <1721610202@outlook.com>
2025-11-08 12:22:33 +0800

    feat: 修复配置文件

--- public/bizConfig.js
+++ public/bizConfig.js
@@ -5,7 +5,7 @@ window.__bizConfig__ = {
   // xxx
   VITE_GLOB_XXX: [166, 176, 1311],
   // # 字典地址配置
-  VITE_GLOB_DICTIONARY_URL: "http://172.16.11.13:32313/spider-fe/dic/list/1341/19/%E9%87%8D%E7%82%B9%E4%BA%BA?hiddenBack=true"
+  VITE_GLOB_DICTIONARY_URL: "http://172.16.11.13:32313/spider-fe/dic/list/1341/19/%E9%87%8D%E7%82%B9%E4%BA%BA?hiddenBack=true",
   // 检测更新定时器
-  VITE_UPDATE_INTERVAL: 10 * 1000
+  VITE_UPDATE_INTERVAL: 10 * 1000,
 }
